485a652e58c96f6b63ba5b6b3cca2ed7b33feefa
Peter Szabo <szwazzup@gmail.com>
2022-03-12 15:39:12 +0100

    Osztószámítás

---
+++ src/hu/progmatic/Main.java
@@ -0,0 +1,27 @@
+package hu.progmatic;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérlek, adj meg egy természetes számot: ");
+        int szam = scanner.nextInt();
+        scanner.nextLine();
+
+        int oszto = 1;
+        int db = 0;
+        while (oszto <= szam) {
+            if (szam % oszto == 0) {
+                db ++;
+                System.out.println("A szám osztója: " + oszto);
+            }
+            oszto ++;
+        }
+
+        System.out.println();
+        System.out.println("A számnak összesen " + db + " db osztója van.");
+        //System.out.println("oszto " + oszto);
+    }
+}
